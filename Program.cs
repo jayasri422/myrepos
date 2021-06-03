b28a356ac5c64e27e329798b62e9cd9b9f98ad00
jayasri422 <68181712+jayasri422@users.noreply.github.com>
2021-06-03 09:23:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d10t1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 23, 11, 19, 55, 67, 83 };
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
