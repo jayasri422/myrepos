7aac35b9a400871f086f7aa05d9d9fecd459d468
jayasri422 <68181712+jayasri422@users.noreply.github.com>
2021-06-03 09:30:45 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using multiplication;
+namespace area
+{
+    public class Class2
+    {
+        public void Square(int side)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine("area:"+(obj.Mul(side, side)));
+        }
+        public void Circle(int radius)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine("area:" +(obj.Mul(radius, radius) *3.14));
+        }
+        public void Triangle(int l,int b)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine("area:" + (obj.Mul(l, b) /2));
+        }
+    }
+}
